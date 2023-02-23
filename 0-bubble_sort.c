260173a9b56926c85911bac846c6f38351516054
Oluwasheyi <jaywoncoded@gmail.com>
2023-02-23 12:48:42 +0000

    Buble Algorithm solved

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,19 +6,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, index, tmp = 0;
+	int i, j, temp;
 
-	if (size < 2)
-		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[j] > array[j + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				/* Print the array after swapping two elements */
+				printf("After swapping %d and %d: ", array[j], array[j + 1]);
+				for (int k = 0; k < size; k++)
+				{
+					printf("%d ", array[k]);
+				}
+				printf("\n");
 			}
 		}
+	}
 }
