628f63d0a3b9888549d6fbb1e49d72af165f51a5
Oluwasheyi <jaywoncoded@gmail.com>
2023-02-24 11:32:35 +0000

    test 7

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,8 +11,8 @@ void bubble_sort(int *array, size_t size)
 
 	if (size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (j = 0; j < size; j++)
+	for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i; j++)
 		{
 			if (array[j] > array[j + 1] && array[j + 1])
 			{
